a862171014cf9cd2ef077880f93e87af363b330c
shadz22 <shadz22@yahoo.co.uk>
2019-12-08 14:07:52 +0000

    Load students list from a file on start up

--- directory.rb
+++ directory.rb
@@ -5,7 +5,7 @@ def interactive_menu
     # print the interactive menu options
     print_menu
     #do what the user has asked
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -13,7 +13,7 @@ def input_student
   puts "please enter the name of the student"
   puts "To finish, just hit return twice"
   # get the first name
-  name = gets.chomp
+  name = STDIN.gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
@@ -21,7 +21,7 @@ def input_student
     puts "Now we have #{@students.count} students"
     # get another name from the user
     puts "Enter another name"
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
 end
 
@@ -84,15 +84,29 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(",")
     @students << {name: name, cohort: cohort.to_sym}
   end 
   file.close
 end
+
+def try_load_students
+  filename = ARGV.first
+    return if filename.nil?
+    if File.exists?(filename)
+      load_students(filename)
+      puts "Loaded #{@students.count} students from #{filename}"
+    else
+      puts "Sotty, #{filename} doesn't exist."
+      exit
+    end
+end
+      
   
   #nothing happens until we call the methods
+try_load_students
 interactive_menu
 
